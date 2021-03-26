a4b1272750da390f6154412fabf75fd8a18f04c4
Navraj Bains <navraj.bains.89@outlook.com>
2021-03-26 13:49:55 -0400

    '?'

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 const dbServe = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-defined-55629',
         user: 'postgres',
         password: '262560',
         database: 'facerec'
